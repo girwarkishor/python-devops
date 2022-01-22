6ee0c9ecfe959d415c2a9fd5b7d0957cb4e27baa
paramesh <parameswara.kuna@gmail.com>
2022-01-22 08:06:31 +0530

    class notes 21/01

--- class17/git_automation.py
+++ class17/git_automation.py
@@ -3,4 +3,38 @@
 
 import git
 
+PATH = "E:\\divyaprojects\\visualpathdevops\\class17\\sample_repo"
+GIT_CLONE_URL = "https://gitlab.com/pavan-projects/april_come.git"
+BRANCH_NAME = "automation_branch"
+
+#clone step
+
+try:
+    repo = git.Repo.clone_from(GIT_CLONE_URL, PATH)
+except git.exc.GitCommandError:
+    repo = git.Repo(PATH)
+
+
+try:
+    repo.git.checkout("-b", "devops_branch")
+except git.exc.GitCommandError:
+    repo.git.checkout("devops_branch")
+
+
+try:
+    repo.git.add("--all")
+    repo.git.commit('-m', "sample commit", author="parameswara.kuna@gmail.com")
+except git.exc.GitCommandError:
+    print("noting to add")
+
+
+
+origin = repo.remote(name="origin")
+origin.push()
+
+
+
+
+
+
 
