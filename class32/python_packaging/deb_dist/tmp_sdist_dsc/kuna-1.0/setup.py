3f2caf94a242ce3a06fb828eeb406434b64c9d2f
parameswara.kuna@gmail.com <parameswara.kuna@gmail.com>
2022-02-12 09:14:13 +0530

    class notes till 12/02

---
+++ class32/python_packaging/deb_dist/tmp_sdist_dsc/kuna-1.0/setup.py
@@ -0,0 +1,10 @@
+import os
+from setuptools import setup
+# from nvpy import nvpy
+
+setup(
+    name = "kuna",
+    version = "1.0",
+    author = "parameswara.kuna@gmail.com",
+    description = "Demo for packaging",
+)
